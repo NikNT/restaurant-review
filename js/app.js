97f748dfc30cf25a3e6eb2d33e9e262418078b21
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2021-05-28 19:52:44 +0530

    First Commit - Home Page

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const switcher = document.querySelector('#theme-switch');
+
+const defaultTheme = localStorage.getItem('theme') || 'light-theme'; 
+setTheme(defaultTheme); 
+
+switcher.addEventListener('change', (e) => {
+    // console.log(e.target.value); 
+    setTheme(e.target.value); 
+});
+
+function setTheme(theme){
+    theme = theme || 'light-theme'; 
+    document.documentElement.className = theme; 
+    
+    localStorage.setItem('theme', theme); 
+    switcher.value = theme; 
+}
+ 
