7a182be44a47f39247715ac3c0f59ec0ca549588
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2021-06-02 21:33:35 +0530

    Pages - Completed

--- js/feedback.js
+++ js/feedback.js
@@ -1,33 +1,7 @@
-//Logic - Theme Switcher 
-
-const switcher = document.querySelector('.theme-switch');
-
-const defaultTheme = localStorage.getItem('theme') || 'light-theme'; 
-setTheme(defaultTheme); 
-
-switcher.addEventListener('change', (e) => {
-    // console.log(e.target.value); 
-    setTheme(e.target.value); 
-});
-
-function setTheme(theme){
-    theme = theme || 'light-theme'; 
-    document.documentElement.className = theme; 
-    
-    localStorage.setItem('theme', theme); 
-    switcher.value = theme; 
-}
-
-
-//Logic - Content Filter 
-
 document.getElementById('submitButton').onclick = function checkComment(){
 
     const prohibitedWords = ['Shit', 'shit', 'SHit', 'SHIt', 'SHIT', 'sHIT', 'Asshole', 
-                              'ASS', 'ass', 'ASshole', 'ASShole', 'ASSHole', 'ASSHOle', 
-                              'Shit.', 'shit.', 'SHit.', 'SHIt.', 'SHIT.', 'sHIT.', 'Asshole.', 
-                              'ASS.', 'ass.', 'ASshole.', 'ASShole.', 'ASSHole.', 'ASSHOle.']
-                              ; 
+                              'ASS', 'ass', 'ASshole', 'ASShole', 'ASSHole', 'ASSHOle']; 
                               
     let count = 0;
 
